5e7ec92bac7d4bdac7a752a683f1a14d1cad25e8
huqc <1026489907@qq.com>
2022-12-16 13:53:18 +0800

    test: add a case for 100% coverage

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,37 @@
-import simpleFormatDate from '../src/index';
+import simpleFormatDate from '../src/index'
 
-test('simpleFormatDate', () => {
-  expect(simpleFormatDate('2021-05-13 17:54:55', 'yyyy-MM-dd')).toBe('2021-05-13');
+describe('simpleFormatDate', () => {
+  test('yyyy-MM-dd', () => {
+    expect(simpleFormatDate('2021-05-13 17:54:55', 'yyyy-MM-dd')).toBe(
+      '2021-05-13'
+    )
+  })
 
-  expect(simpleFormatDate('2021-05-13 17:54:55', 'yyyy-MM-dd HH:mm:ss')).toBe('2021-05-13 17:54:55');
+  test('yyyy-MM-dd HH:mm:ss', () => {
+    expect(simpleFormatDate('2021-05-13 17:54:55', 'yyyy-MM-dd HH:mm:ss')).toBe(
+      '2021-05-13 17:54:55'
+    )
+  })
 
-  const date = new Date();
-  const month = date.getMonth() + 1;
-  const formatMonth = month >= 10 ? month : `0${month}`
-  const str = `${date.getFullYear()}-${formatMonth}-${date.getDate()}` ;
-  expect(simpleFormatDate('', 'yyyy-MM-dd')).toBe(str);
+  test('current format time', () => {
+    const date = new Date()
+    const month = date.getMonth() + 1
+    const formatMonth = month >= 10 ? month : `0${month}`
+    const str = `${date.getFullYear()}-${formatMonth}-${date.getDate()}`
 
-  expect(simpleFormatDate('aa', 'yyyy-MM-dd')).toBe('aa');
-});
+    expect(simpleFormatDate('', 'yyyy-MM-dd')).toBe(str)
+  })
+
+  test('default time', () => {
+    const date = new Date()
+    const month = date.getMonth() + 1
+    const formatMonth = month >= 10 ? month : `0${month}`
+    const str = `${date.getFullYear()}-${formatMonth}-${date.getDate()}`
+
+    expect(simpleFormatDate()).toBe(str)
+  })
+
+  test('Invalid Date', () => {
+    expect(simpleFormatDate('aa', 'yyyy-MM-dd')).toBe('aa')
+  })
+})
